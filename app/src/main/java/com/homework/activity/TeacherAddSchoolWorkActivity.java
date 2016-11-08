eb7875a68b091036f1818dbe0ed118fdacb78f98
wangrongjun <15521302230@163.com>
2016-11-09 01:20:26 +0800

    正在做教师发布作业页面

--- app/src/main/java/com/homework/activity/TeacherAddSchoolWorkActivity.java
+++ app/src/main/java/com/homework/activity/TeacherAddSchoolWorkActivity.java
@@ -1,11 +1,13 @@
 package com.homework.activity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.homework.R;
@@ -17,6 +19,7 @@ import com.wang.java_util.TextUtil;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -33,16 +36,21 @@ public class TeacherAddSchoolWorkActivity extends BaseActivity {
     BorderEditText etName;
     @Bind(R.id.et_content)
     BorderEditText etContent;
-    @Bind(R.id.et_final_date)
-    BorderEditText etFinalDate;
     @Bind(R.id.btn_select_file)
     Button btnSelectFile;
     @Bind(R.id.btn_add_school_work)
     Button btnAddSchoolWork;
     @Bind(R.id.tv_file_path_list)
     TextView tvFilePathList;
+    @Bind(R.id.btn_final_date)
+    TextView btnFinalDate;
+    @Bind(R.id.btn_final_time)
+    TextView btnFinalTime;
 
     private List<String> filePathList;
+
+    private SimpleDateFormat sdf;
+    private Calendar calendar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,15 +58,21 @@ public class TeacherAddSchoolWorkActivity extends BaseActivity {
         setContentView(R.layout.activity_teacher_add_school_work);
         ButterKnife.bind(this);
 
-        filePathList = new ArrayList<>();
+        initData();
         initView();
+    }
+
+    private void initData() {
+        filePathList = new ArrayList<>();
+        calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000);//一周后
+        sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+//        sdf.format(new Date(calendar.getTimeInMillis()));
     }
 
     private void initView() {
         etContent.setText("没有作业内容");
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        Date date = new Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000);//一周后
-        etFinalDate.setText(sdf.format(date));
+//        tvFinalDate.setText(sdf.format(date));
     }
 
     private void updateFileListView() {
@@ -68,16 +82,22 @@ public class TeacherAddSchoolWorkActivity extends BaseActivity {
         }
     }
 
-    @OnClick({R.id.btn_select_file, R.id.btn_add_school_work})
+    @OnClick({R.id.btn_select_file, R.id.btn_add_school_work, R.id.btn_final_date, R.id.btn_final_time})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_select_file:
                 startActivityForResult(IntentUtil.getFileIntent(), 0);
                 break;
+            case R.id.btn_final_date:
+                showDatePickerDialog();
+                break;
+            case R.id.btn_final_time:
+                showTimePickerDialog();
+                break;
             case R.id.btn_add_school_work:
                 String name = etName.getText();
                 String content = etContent.getText();
-                String finalDate = etFinalDate.getText();
+                String finalDate = btnFinalDate.getText().toString();
                 if (!TextUtil.isEmpty(name, content, finalDate)) {
                     if (name.length() > 36) {
                         M.t(this, "作业标题不能超过36个字符");
@@ -91,6 +111,23 @@ public class TeacherAddSchoolWorkActivity extends BaseActivity {
                 }
                 break;
         }
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                calendar.set(year, monthOfYear, dayOfMonth);
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                btnFinalDate.setText(sdf.format(new Date(calendar.getTimeInMillis())));
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        dialog.setTitle("请选择截止日期");
+        dialog.show();
+    }
+
+    private void showTimePickerDialog() {
+
     }
 
     private void startAddSchoolWork(String name, String content, String finalDate) {
